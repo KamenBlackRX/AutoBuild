3e3ffdb132b82563f6947d1aa1a7a380f93bd51a
Jefferson Puchalski <puchiruzuki@gmail.com>
2018-10-17 13:52:02 -0300

    Fix somes errors in main, added Pyside2 suport and added wizard ui file for loading

--- core/gui/gtk/window.py
+++ core/gui/gtk/window.py
@@ -13,3 +13,22 @@ class Window(Gtk.Window):
 
     def createMainWindow(defaultButtons = True):
         pass;
+
+class MainWindow(Gtk.Window):
+
+    def __init__(self):
+        Gtk.Window.__init__(self, title="Autobuild")
+        # add a box to hold all
+        self.hbox = Gtk.Box()
+
+        self.button = Gtk.Button(label="Start Autobuild")
+        self.button.connect("clicked", self.on_button_clicked)
+   
+
+        #Add Label
+        self.label = Gtk.Label()
+        self.label.set_label = "The Auto Build program for Fast Enviroment Configuration"
+        self.add(self.hbox)
+
+    def on_button_clicked(self, widget):
+        print("Started Autobuild")
--- main.py
+++ main.py
@@ -1,34 +1,90 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Autobuild
+
 import os
-import gi
+import sys
+import PySide2.QtCore
+import random
+from PySide2 import QtCore, QtWidgets, QtGui, QtUiTools
+import threading
 
-from gi.repository import Gtk
-gi.require_version('Gtk', '3.0')
-
-
-class MainWindow(Gtk.Window):
-
+class Utils(object):
     def __init__(self):
-        Gtk.Window.__init__(self, title="Autobuild")
-        # add a box to hold all
-        self.hbox = Gtk.Box()
+        pass
 
-        self.button = Gtk.Button(label="Start Autobuild")
-        self.button.connect("clicked", self.on_button_clicked)
-   
+    def loadUiForm(self, uiPath):
+        loader = QtUiTools.QUiLoader()
+        file = QtCore.QFile(uiPath)
+        file.open(QtCore.QFile.ReadOnly)
+        LoadedWidget = loader.load(file, self)
+        file.close()
+        return LoadedWidget
 
-        #Add Label
-        self.label = Gtk.Label()
-        self.label.set_label = "The Auto Build program for Fast Enviroment Configuration"
-        self.add(self.hbox)
+class WizardWindow(QtWidgets.QWidget):
+    def __init__(self):
+        pass
 
-    def on_button_clicked(self, widget):
-        print("Started Autobuild")
+class MainWindow(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+        self.counter = 0
+        self.hello = [
+            "Welcome to AutoBuild System",
+            "Bem vindo ao Autobuild System",
+            "アウトブイォえうこそ"
+        ]
 
+        self.button = QtWidgets.QPushButton("Click here for start!")
+        self.text = QtWidgets.QLabel(self.hello[0])
+        self.text.setAlignment(QtCore.Qt.AlignCenter)
+
+        self.text.setFont(QtGui.QFont("Titillium", 30))
+        self.button.setFont(QtGui.QFont("Titillium", 20))
+
+        self.layout = QtWidgets.QVBoxLayout()
+        self.layout.addWidget(self.text)
+        self.layout.addWidget(self.button)
+        self.setLayout(self.layout)
+
+        self.button.clicked.connect(self.startProcess)
+        timer = QtCore.QTimer(self)
+        timer.timeout.connect(self.magic)
+        timer.start(3000)
+                           
+
+    def startProcess(self):
+        print('start')
+
+    def magic(self):
+        if self.counter >= len(self.hello):
+            self.counter = 0
+        self.text.setText(self.hello[self.counter])
+        self.counter +=1
+        
 if __name__ == "__main__":
-    print("Stating from new")
- 
+    # Prints PySide version
+    # e.g. 1.0.2
+    print('PySide2 Version:', PySide2.__version__)
 
-    win = MainWindow()
-    win.connect("destroy", Gtk.main_quit)
-    win.show_all()
-    Gtk.main()
+    # Gets a tuple with each version component
+    # e.g. (1, 0, 2, 'final', 1)
+    print('PySide2 Final Version:',PySide2.__version_info__)
+
+    # Prints the Qt version used to compile PySide
+    # e.g. "5.11.0"
+    print('PySide2.QtCore Version:',PySide2.QtCore.__version__)
+
+    # Gets a tuple with each version components of Qt used to compile PySide
+    # e.g. (5, 11, 0)
+    print('PySide2.QtCore Compile Version:',PySide2.QtCore.__version_info__)
+
+
+    app = QtWidgets.QApplication([])
+
+    widget = MainWindow()
+    widget.resize(800, 600)
+    widget.show()
+
+    sys.exit(app.exec_())
